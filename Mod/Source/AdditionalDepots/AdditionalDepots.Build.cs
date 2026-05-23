a3d930bea70041d00497c0dd17cf103f561940e9
Jarno <jarno.westhof@gmail.com>
2026-05-23 19:40:21 +0200

    Fixed bug with missing resources in build gun

--- Mod/Source/AdditionalDepots/AdditionalDepots.Build.cs
+++ Mod/Source/AdditionalDepots/AdditionalDepots.Build.cs
@@ -7,27 +7,10 @@ public class AdditionalDepots : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 		CppStandard = CppStandardVersion.Cpp20;
 		bUseUnity = false;
-		OptimizeCode = CodeOptimization.Never;
+		OptimizeCode = CodeOptimization.InShippingBuildsOnly;
 
-        // FactoryGame transitive dependencies
-        // Not all of these are required, but including the extra ones saves you from having to add them later.
-        // Some entries are commented out to avoid compile-time warnings about depending on a module that you don't explicitly depend on.
-        // You can uncomment these as necessary when your code actually needs to use them.
-        PublicDependencyModuleNames.AddRange(new string[] {
-			"Core", "CoreUObject",
-			"Engine",
-			"InputCore",
-			"GameplayTasks",
-			"SlateCore", "Slate", "UMG",
-			"NetCore",
-			"ReliableMessaging"
-        });
-
-		// Header stubs
-		PublicDependencyModuleNames.AddRange(new string[] {
-			"DummyHeaders",
-		});
-
-		PublicDependencyModuleNames.AddRange(new string[] {"FactoryGame", "SML"});
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "ReliableMessaging" });
+		PublicDependencyModuleNames.AddRange(new string[] { "DummyHeaders" });
+		PublicDependencyModuleNames.AddRange(new string[] { "FactoryGame", "SML" });
 	}
 }
--- Tests/Source/AdditionalDepotsTest/AdditionalDepotsTest.Build.cs
+++ Tests/Source/AdditionalDepotsTest/AdditionalDepotsTest.Build.cs
@@ -9,25 +9,14 @@ public class AdditionalDepotsTest : ModuleRules
 		bUseUnity = false;
 		OptimizeCode = CodeOptimization.Never;
 
-        PublicDependencyModuleNames.AddRange(new string[] {
-			"Core", "CoreUObject",
-			"Engine",
-			"InputCore",
-			"GameplayTasks",
-			"SlateCore", "Slate", "UMG",
-			"NetCore",
-        });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine" });
 
 		// Header stubs
-		PublicDependencyModuleNames.AddRange(new string[] {
-			"DummyHeaders",
-		});
+		PublicDependencyModuleNames.AddRange(new string[] { "DummyHeaders" });
 
 		// Mod code depdencies
-        PrivateDependencyModuleNames.AddRange(new string[] { 
-            "AdditionalDepots"
-        });
+        PrivateDependencyModuleNames.AddRange(new string[] { "AdditionalDepots" });
 
-		PublicDependencyModuleNames.AddRange(new string[] {"FactoryGame", "SML"});
+		PublicDependencyModuleNames.AddRange(new string[] { "FactoryGame", "SML" });
 	}
 }
